52769d0be1dd4fbc1bdf34312d269951274ca68c
BaxterTU2014 <baxterTU2014@gmail.com>
2014-10-10 16:42:50 +0200

    Create LampController.py

---
+++ LampController.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import rospy
+from std_msgs.msg import String
+import baxter_interface
+
+class LampController():
+
+    def __init__(self):
+        rospy.init_node('LampController')
+        #load saved actions
+
+    def toggleLeftGripper(self):
+        pass
+
+    def rotateLeftGripper(self):
+        pass
+
+    def rotateLeftGripper(self):
+        pass
+
+    def recordNewPickup(self):
+        pass
+
+    def playBackPickup(self):
+        pass
+
+
+
+
+
+    def callback(self, data):
+
+
